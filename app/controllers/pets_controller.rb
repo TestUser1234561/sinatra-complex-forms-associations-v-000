e105f7be7c65bc10c446942bffe102d4db4f3557
Morgan Fudge <mfudge92@gmail.com>
2018-01-31 02:20:33 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,7 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"].empty?
+      @pet.owner = Owner
     if !params["owner_name"].empty?
       @pet.owner << Owner.create(name: params["owner_name"])
     end
