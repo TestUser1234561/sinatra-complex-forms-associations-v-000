1baf563f50eac00e5e10cb52861cc5600c8830c6
Morgan Fudge <mfudge92@gmail.com>
2018-01-31 02:25:02 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,7 @@ class PetsController < ApplicationController
     if !params["owner"]
       @pet.owner = Owner.find_by(id: params["owner"])
     else !params["owner_name"].empty?
-      @pet.owner << Owner.create(name: params["owner_name"])
+      @pet.owner = Owner.create(name: params["owner_name"])
       @pet.owner.save
     end
     @pet.save
