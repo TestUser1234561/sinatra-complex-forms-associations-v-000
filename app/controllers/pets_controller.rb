694421a15a0af67bf017db725ab4667c81e2f142
Morgan Fudge <mfudge92@gmail.com>
2018-01-31 02:47:50 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,7 +35,8 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if params["owner"]
-      owner = Owner.find_by(name: params["owner"]["name"]) ? Owner.find_by(name: params["owner"]["name"]) : Owner.create(name: params["owner"]["name"])
+      owner? = Owner.find_by(name: params["owner"]["name"])
+      owner = owner? ? owner? : Owner.create(name: params["owner"]["name"])
       @pet.update(owner: owner)
     end
     redirect to "pets/#{@pet.id}"
